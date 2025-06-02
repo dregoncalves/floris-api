e5ab87e9dfb8db8cc77c85468394f9c2f2370362
Andre Gonçalves <dregoncalves22@gmail.com>
2025-06-02 19:14:27 -0300

    Initial commit

---
+++ src/main/java/br/com/floris/FlorisApplication.java
@@ -0,0 +1,13 @@
+package br.com.floris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/floris/FlorisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.floris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
