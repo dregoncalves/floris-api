72b7e6450aa30df8fadf9401127abe77ddb53002
Andre Gonçalves <dregoncalves22@gmail.com>
2025-06-16 20:08:03 -0300

    refactor: implementa autenticação usando cookies

--- src/main/java/br/com/floris/repository/UserRepository.java
+++ src/main/java/br/com/floris/repository/UserRepository.java
@@ -9,7 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
-
+    
     Optional<User> findByUsernameOrEmail(String username, String email);
 
     boolean existsByUsernameOrEmail(String username, String email);
