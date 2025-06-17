9bfeba6fcab9fc4a35e021fcd5b627ec2e5e8b76
Andre Gonçalves <dregoncalves22@gmail.com>
2025-06-16 22:56:41 -0300

    update

--- src/main/java/br/com/floris/repository/EntradaRepository.java
+++ src/main/java/br/com/floris/repository/EntradaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +14,10 @@ import java.util.Optional;
 public interface EntradaRepository extends JpaRepository<Entrada, Long> {
 
     Page<Entrada> findByUsuarioId(Long usuarioId, Pageable pageable);
-    
+
     List<Entrada> findAllByUsuarioId(Long usuarioId);
 
     Optional<Entrada> findByIdAndUsuarioId(Long id, Long usuarioId);
+
+    List<Entrada> findAllByUsuarioIdAndDataRecebimentoBetween(Long usuarioId, LocalDate dataInicio, LocalDate dataFim);
 }
--- src/main/java/br/com/floris/repository/GastoRepository.java
+++ src/main/java/br/com/floris/repository/GastoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +19,6 @@ public interface GastoRepository extends JpaRepository<Gasto, Long> {
     List<Gasto> findAllByUsuarioId(Long usuarioId);
 
     Optional<Gasto> findByIdAndUsuarioId(Long id, Long usuarioId);
+
+    List<Gasto> findAllByUsuarioIdAndDataVencimentoBetween(Long usuarioId, LocalDate dataInicio, LocalDate dataFim);
 }
