64f0c15e381741a52b0e3bb2d5b2b234a7bbe148
Andre Gonçalves <dregoncalves22@gmail.com>
2026-01-13 15:15:55 -0300

    refactor: altera nome da tabela para 'users' devido a palavra reservada no postgres

--- src/main/java/br/com/floris/model/User.java
+++ src/main/java/br/com/floris/model/User.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table(name = "user")
+@Table(name = "users")
 @Entity(name = "user")
 @Getter
 @Setter
